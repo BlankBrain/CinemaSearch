b39f70f0a974acf3a28bde6be1bce70649e5d486
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2021-05-06 22:49:09 +0600

    movie model decodable
    movie model added using decodable struct

--- CinemaTest/CinemaTest/MovieListVC.swift
+++ CinemaTest/CinemaTest/MovieListVC.swift
@@ -6,20 +6,40 @@
 //
 
 import UIKit
+import Alamofire
 
 class MovielistVC: UIViewController, UITableViewDelegate , UITableViewDataSource {
    
 
     @IBOutlet weak var tableView: UITableView!
     let cellIdentifiar : String = "cell"
+    let decoder = JSONDecoder()
     var movies = [Movie]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        
+        // af reqq
+        let Url = "https://api.themoviedb.org/3/search/movie?api_key=38e61227f85671163c275f9bd95a8803&query=marvel"
+        AF.request(Url).responseJSON{ response in
+            
+                    do{
+                        print(response)
 
+                     
+                        let Result = try self.decoder.decode(Movie.self , from: response.data! )
+                        
+                        print(Result)
+                        
+                    }catch{
+                        print("error parsing json data")  }
+
+                    }
+    }
+   
     
    
     
--- CinemaTest/CinemaTest/MovieModel.swift
+++ CinemaTest/CinemaTest/MovieModel.swift
@@ -7,30 +7,89 @@
 
 import Foundation
 
+
 // MARK: - Movie
-struct Movie {
+struct Movie :Decodable  {
     let page: Int
     let results: [Result]
-    let totalPages, totalResults: Int
+    let totalPages : Int
+    let totalResults: Int
+    init(
+        page: Int?,
+        results:  [Result] = [],
+        totalPages: Int?,
+        totalResults: Int?
+    
+    ){
+        self.page = page ?? 1
+        self.results = results
+        self.totalPages = totalPages ?? 1
+        self.totalResults = totalResults ?? 1
+    }
 }
 
 // MARK: - Result
-struct Result {
-    let adult: Bool
+struct Result :Decodable{
+    let adult: Int
     let backdropPath: String?
-    let genreIDS: [Int]
+    let genreIDS: [genreIDS]
     let id: Int
     let originalLanguage: OriginalLanguage
-    let originalTitle, overview: String
-    let popularity: Double
+    let originalTitle : String?
+    let overview: String?
+    let popularity: Double?
     let posterPath: String?
-    let releaseDate, title: String
+    let releaseDate : String?
+    let title: String?
     let video: Bool
-    let voteAverage: Double
-    let voteCount: Int
+    let voteAverage: Double?
+    let voteCount: Int?
+    
+    init(
+        adult: Int?,
+        backdropPath: String?,
+        genreIDS:  [genreIDS] = [],
+        id: Int?,
+        originalLanguage: OriginalLanguage,
+        originalTitle: String?,
+        overview: String?,
+        popularity: Double?,
+        posterPath: String?,
+        releaseDate: String?,
+        title: String?,
+        video: Bool?,
+        voteAverage: Double?,
+        voteCount: Int?) {
+        self.adult = adult ?? 0
+        self.backdropPath = backdropPath
+        self.genreIDS = genreIDS
+        self.id = id ?? 404
+        self.originalLanguage = originalLanguage
+        self.originalTitle = originalTitle
+        self.overview = overview
+        self.popularity = popularity
+        self.posterPath = posterPath
+        self.releaseDate = releaseDate
+        self.title = title
+        self.video = video ?? false
+        self.voteAverage = voteAverage
+        self.voteCount = voteCount
+
+    }
 }
 
-enum OriginalLanguage {
-    case en
+enum OriginalLanguage: String, Decodable  {
+    case en, other
 }
+struct genreIDS :Decodable {
+    
+        let genreID: Int?
+    init(
+        genreID: Int?){
+        self.genreID = genreID
+
+    }
+}
+
+
 
