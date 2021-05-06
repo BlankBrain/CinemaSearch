9733065db7e0a8d51da8279fb5bbb0c4fc62a46f
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2021-05-06 21:14:01 +0600

    initial commit

---
+++ CinemaTest/CinemaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaTest
+//
+//  Created by Md. Mehedi Hasan on 6/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
